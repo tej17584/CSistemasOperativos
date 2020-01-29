a2bf6f61bbf8a699f444172e0db54ead0f773bfc
Alejandro Tejada <atejada@pacifiko.com>
2020-01-28 21:02:27 -0600

    constantes

---
+++ variables/constantes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main(int argc, char const *argv[])
+{
+    //Para definir constantes, usamos define
+#define PI 3.14159265
+
+    printf("%f\n", PI);
+    printf("%f\n", M_PI);
+    return 0;
+}
