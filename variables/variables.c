c04b81be1047e51f5759b03f00236f866f703d3b
Alejandro Tejada <atejada@pacifiko.com>
2020-01-28 20:38:36 -0600

    variables

---
+++ variables/variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    double castillo;
+    double grandTotal;
+
+    grandTotal = 500.50;
+    castillo = 10.5;
+    printf("castillo=%.2f\n", castillo);
+    castillo = 100.25;
+    printf("castillo 2=%.2f\n", castillo);
+    printf("grand total=%.2f\n", grandTotal);
+    return 0;
+}
